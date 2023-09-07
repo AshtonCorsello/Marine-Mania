1133307b3a84bfb8aca8459ff1b1b24e6a097da7
Toby <131214504+tbaker60@users.noreply.github.com>
2023-09-07 15:10:27 -0400

    added wasd to movement

--- src/main.js
+++ src/main.js
@@ -19,22 +19,22 @@ class Player {
 }
 
 function movePlayer(event) {
-    if (event.keyCode == 37) { // left arrow
+    if (event.keyCode == 37 || event.keyCode == 65) { // left arrow / a
         player.x -= 10; // move the player left
         if (player.x < 0) { //Don't let the player go off the screen
             player.x = 0;
         }
-    } else if (event.keyCode == 39) { // right arrow
+    } else if (event.keyCode == 39 || event.keyCode == 68) { // right arrow / d
         player.x += 10;
         if (player.x > CANV_WIDTH - player.size) {
             player.x = CANV_WIDTH - player.size;
         }
-    } else if (event.keyCode == 38) { // up arrow
+    } else if (event.keyCode == 38 || event.keyCode == 87) { // up arrow / w
         player.y -= 10;
         if (player.y < (CANV_HEIGHT - (CANV_HEIGHT / 8))) { //Don't let the player go above 1/8 of the screen
             player.y = (CANV_HEIGHT - (CANV_HEIGHT / 8));
         }
-    } else if (event.keyCode == 40) { // down arrow
+    } else if (event.keyCode == 40 || event.keyCode == 83) { // down arrow / s
         player.y += 10;
         if (player.y >= CANV_HEIGHT) {
             player.y = CANV_HEIGHT - player.size;
