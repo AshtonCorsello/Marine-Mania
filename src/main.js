056d40592961dbb5981d9ed89cbefa4a502b0279
Toby <131214504+tbaker60@users.noreply.github.com>
2023-09-12 16:55:54 -0400

    Working on adding projectiles

--- src/main.js
+++ src/main.js
@@ -82,17 +82,19 @@ let player; // player object
 let pressedKeys = {}; // Holding for the pressed keys
 
 let enemies = []; // array to hold snowflake objects
+let projectiles = []; // array to hold projectile objects
 
 function setup() {
     createCanvas(CANV_WIDTH, CANV_HEIGHT);
     fill(240);
     noStroke();
     player = new Player(CANV_WIDTH/2,(CANV_HEIGHT - CANV_HEIGHT/16),10); // create a new player object
-    enemy1 = new Enemy1()
+    enemy1 = new Enemy1();
+    projectile1 = new Projectile();
 }
 
 function draw() {
-    background(145, 240, 243); // set the background to white
+    background(145, 240, 243); // set the background to blue
     textSize(18); // determines size of font
     fill(51); // determines color of text
 
@@ -100,6 +102,7 @@ function draw() {
 
     player.update();
     enemy1.showcase();
+    projectile1.showcase();
 }
 
 function keyPressed(){
@@ -167,84 +170,50 @@ class Enemy1 {
 }
 
 // Need to look through this to develop projectiles
-/*class Enemy1 {
+class Projectile {
 
-    constructor() {
-      // initialize coordinates
-      this.posX = 0;
-      this.posY = random(-50, 0);
-      this.initialangle = random(0, 2 * PI);
-      this.size = 15;
-
-      // radius of placeholder
-      this.radius = sqrt(random(pow(width / 2, 2)));
-
+    constructor(targetX, targetY) {
+        // initialize coordinates
+        this.posX = player.x;
+        this.posY = player.y + player.size/2;
+        this.initialAngle = atan((targetY-player.y)/(targetX-player.x));
+        console.log(this.initialAngle);
+        this.size = 10;
     }
   
   
     update(time) {
       // x position follows a circle
       let w = 0.6; // angular speed
-      let angle = w * time + this.initialangle;
+      let angle = w * time + this.initialAngle;
       this.posX = width / 2 + this.radius * sin(angle);
-        this.posY += pow(this.size, 0.5);
+      this.posY += pow(this.size, 0.5);
   
       // delete enemy if past end of screen
       if (this.posY > height) {
-        let index = enemies.indexOf(this);
-        enemies.splice(index, 1);
+        let index = projectiles.indexOf(this);
+        projectiles.splice(index, 1);
       }
-    };
-  
+    }
+
     display() {
       ellipse(this.posX, this.posY, this.size);
-    };
+    }
 
     showcase() {
-      const delay = random (1000, 5000) //ms
-      if(!this.task_done) {
-          enemies.push(new Enemy1()); // append enemy object
-          this.task_done = true;
-          this.last_done = millis();
-      }
-      else {
-          if(millis() - this.last_done > delay) {
-            this.task_done = false;
-          }
-      } 
+      const delay = 2500 //ms
       let t = frameCount / 60; // update time
 
-     // loop through enemies with a for..of loop
-      for (let enmy of enemies) {
-         enmy.update(t); // update enemy position
-         enmy.display(); // draw enemy
+     // loop through projectiles with a for..of loop
+      for (let prjctl of projectiles) {
+         prjctl.update(t); // update projectile position
+         prjctl.display(); // draw projectile
       }
     }
 }
-
-// want to combine the events with mouse presses for shooting projectiles
-function movePlayer(event) {
-    if (event.keyCode == 37 || event.keyCode == 65) { // left arrow / a
-        player.x -= 10; // move the player left
-        if (player.x < player.size) { //Don't let the player go off the screen
-            player.x = player.size;
-        }
-    } else if (event.keyCode == 39 || event.keyCode == 68) { // right arrow / d
-        player.x += 10;
-        if (player.x > CANV_WIDTH - player.size) {
-            player.x = CANV_WIDTH - player.size;
-        }
-    } else if (event.keyCode == 38 || event.keyCode == 87) { // up arrow / w
-        player.y -= 10;
-        if (player.y < (CANV_HEIGHT - (CANV_HEIGHT / 3))) { //Don't let the player go above 1/3 of the screen
-            player.y = (CANV_HEIGHT - (CANV_HEIGHT / 3));
-        }
-    } else if (event.keyCode == 40 || event.keyCode == 83) { // down arrow / s
-        player.y += 10;
-        if (player.y >= CANV_HEIGHT) {
-            player.y = CANV_HEIGHT - player.size;
-        }
-    }
-}
-*/
+  // want to combine the events with mouse presses for shooting projectiles
+  function mouseClicked(){
+      console.log("Firing");
+      projectiles.push(new Projectile(mouseX, mouseY));
+  }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
