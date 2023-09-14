66e1368e5652a752e9a152722417faae7f213c99
Toby <131214504+tbaker60@users.noreply.github.com>
2023-09-14 12:02:00 -0400

    Made projectiles scalable with canvas size

--- src/main.js
+++ src/main.js
@@ -171,34 +171,36 @@ class Enemy1 {
 
 // Need to look through this to develop projectiles
 class Projectile {
+    
 
     constructor(targetX, targetY) {
         // initialize coordinates
         this.posX = player.x;
         this.posY = player.y + player.size/2;
-        //this.dirMult = 1;
+        
+        this.projectileScalar = 28800; //currently set to scale projectiles to 10px and 10px per frame on 720 x 400 canvas
+      
+        // Setting up movement of the projectiles
         this.dirMult = (targetX<player.x ? -1: 1);
         this.slope = this.dirMult * (player.y-targetY)/(player.x-targetX);
-        this.speed = 10;
+        this.speed = (CANV_WIDTH*CANV_HEIGHT)/this.projectileScalar;
         angleMode(DEGREES);
         this.initialAngle = atan(this.slope);
+     /* debug messages
         console.log("Target: " , targetX , "," , targetY);
         console.log("Player: " , player.x , "," , player.y);
         console.log(this.initialAngle);
-        this.size = 10;
+     */
+        this.size = (CANV_WIDTH*CANV_HEIGHT)/this.projectileScalar;
     }
   
   
     update(time) {
-      // x position follows a circle
-      //let w = 0.6; // angular speed
-      //let angle = w * time + this.initialAngle;
-      //this.posX += this.dirMult;
-      //this.posY += this.slope;
+      // Decides how much to move in each direction per frame
       this.posY += sin(this.initialAngle)*this.speed;
       this.posX += cos(this.initialAngle)*this.dirMult*this.speed;
   
-      // delete enemy if past end of screen
+      // delete projectile if past end of screen
       if (this.posY > height) {
         let index = projectiles.indexOf(this);
         projectiles.splice(index, 1);
@@ -206,12 +208,14 @@ class Projectile {
     }
 
     display() {
+      stroke(255, 200, 222);
+      fill(55);
       ellipse(this.posX, this.posY, this.size);
     }
 
     showcase() {
       const delay = 2500 //ms
-      let t = frameCount / 120; // update time
+      let t = frameCount / 60; // update time
 
      // loop through projectiles with a for..of loop
       for (let prjctl of projectiles) {
@@ -222,7 +226,7 @@ class Projectile {
 }
   // want to combine the events with mouse presses for shooting projectiles
   function mouseClicked(){
-      console.log("Firing");
+      //console.log("Firing");
       projectiles.push(new Projectile(mouseX, mouseY));
   }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
