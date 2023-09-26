dbf5fccc0c49a163f8d778532b107ac9eef832e3
CJ-Varga <cjvargabusiness@gmail.com>
2023-09-26 16:46:33 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,76 @@
 const CANV_WIDTH = 720; 
 const CANV_HEIGHT = 400;
+//var score = 0; // Used to keep track of player score
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////  PLAYER CLASS AND FUNCTIONS  /////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const MIN_ENMY_DELAY = 50; // least possible spawn delay for enemies in miliseconds
-const STARTING_ENMY_DELAY = 1000;
-const DELAY_DECR_MULT = 10; //how fast level progresses //dont use large number
+class Player {
+   constructor(x, y, size) {
+        this.x = x; // x position of the player
+        this.y = y; // y position of the player
+        this.size = size; // size of the player
+        this.speed = 3;
+        this.score = 0; // Used to keep track of player score
+        this.task_done = false;
+        this.last_done = 0;
+    }
+  
+    update(){
+        let mvmt = createVector(0,0);
+
+        if(pressedKeys.a || pressedKeys.ArrowLeft) {
+            mvmt.x -= 1;
+            if (player.x < 0) { //Don't let the player go off the screen
+                mvmt.x = 0;
+            }
+          }
+          if(pressedKeys.d || pressedKeys.ArrowRight) {
+            mvmt.x += 1;
+            if (player.x > CANV_WIDTH - player.size) {
+                mvmt.x = 0;
+            }
+          }
+          if(pressedKeys.w || pressedKeys.ArrowUp) {
+            mvmt.y -= 1;
+            if (player.y < (CANV_HEIGHT - (CANV_HEIGHT / 4))) { //Don't let the player go above 1/8 of the screen
+                player.y = (CANV_HEIGHT - (CANV_HEIGHT / 4));
+            }
+          }
+          if(pressedKeys.s || pressedKeys.ArrowDown) {
+            mvmt.y += 1;
+            if (player.y >= CANV_HEIGHT - player.size) {
+                player.y = CANV_HEIGHT - player.size;
+            }
+          }
+
+        mvmt.setMag(this.speed); // Limits diagonal speed to still max at 10
+        this.x += mvmt.x;
+        this.y += mvmt.y;
+    }
+  
+    display() { //Draws the player
+      //Draws wake behind boat
+        stroke(255,255,250); //Outline color
+        fill(220, 250, 253); //Color of shape
+        triangle(this.x-this.size/1.5, this.y+4*this.size, this.x, this.y, this.x+this.size/1.5, this.y+4*this.size);
+      //Draws boat
+        stroke(255,255,2); //Outline color
+        fill(226, 194, 162); //Color of shape
+        ellipse(this.x, this.y, this.size*1.75, this.size*4, 3);
+        
+      // details of boat
+        stroke(90);
+        fill(200);
+        quad(this.x-this.size/3, this.y+this.size, this.x+this.size/3, this.y+this.size, this.x+this.size/3, this.y-this.size*.75, this.x-this.size/3, this.y-this.size*.75);
+        stroke(100);
+        fill(190);
+        rectMode(CENTER);
+        square(this.x, this.y+this.size/4, this.size/3);
+
+        text('Score: ' + this.score, 0, 15);// determines what is displayed, at what x,y
+    }
+}
 
 var mode = 0; // Stores weither the user has left the main menu
 let loadTime = 3; // Stores the number of seconds to load
@@ -12,6 +79,7 @@ let loadTime = 3; // Stores the number of seconds to load
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////  P5JS MAIN FUNCTIONS  //////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 let player; // player object
 let pressedKeys = {}; // Holding for the pressed keys
 let enemies = []; // array to hold enemy objects
