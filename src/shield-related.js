d349c9ccbc35ab1c9735da8e865734f9e3e754c1
RonganBai <140551985+RonganBai@users.noreply.github.com>
2023-10-17 18:07:15 -0400

    Add files via upload

--- src/shield-related.js
+++ src/shield-related.js
@@ -3,8 +3,8 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function energie(){// Generate an energy block every 5 seconds
-  if(energies<10 && player.shield == false){
-    energiesarray[energies] = 75+energies*32;
+  if(energies<10 && prop == false){
+    energiesarray[energies] = CANV_HEIGHT*(37/400)+energies*CANV_HEIGHT*(17/400);
     energies++;
     ShieldCT = ShieldCT + 5;
     setTimeout(energie, 5000);
@@ -12,8 +12,7 @@ function energie(){// Generate an energy block every 5 seconds
 }
 
 function Shieldtime(){// Turns off invincibility mode at the end of the energy shield's duration and changes the shield's state
-  player.shield = false
-  player.display();
+  player.display(prop = false);
   mode = 1;
   setTimeout(energie, 5000);
 }
@@ -27,26 +26,10 @@ function ShieldCountdown(){ //Shield Countdown
 
 function OpenShield(){ //  Open Shield
   removeElements(button3); // Disable Shield Button
-  player.shield = true
-  player.display();// Change shield status and display shield
+  player.display(prop = true);// Change shield status and display shield
   mode = 5; //Toggle Invincible Mode
   ShieldCountdown();// Shield Countdown
   setTimeout(Shieldtime, 5000*(energies));// Shield Duration
   energies = 0;// Empty energy
   energiesarray = [];// Empty energy
-}
-
-function displayShieldInfo() {
-   //Draws energy tanks
-   stroke(58, 127, 214);
-   fill(205, 205, 205);
-   rect(CANV_WIDTH*(677/720), CANV_HEIGHT*(116/400), CANV_WIDTH*(30/720), CANV_HEIGHT*(176/400), 5);
- 
- // Draws Energy Blocks
-   for(var i = 0; i < 10; i++){
-     stroke(0,0,0);
-     fill(255, 156, 51);
-     rect(CANV_WIDTH*(677/720), energiesarray[i], CANV_WIDTH*(30/720), CANV_HEIGHT*(15/400), 20);
-   }
- 
 }
