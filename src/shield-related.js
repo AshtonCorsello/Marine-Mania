94248f254114b749753effd0bbdc12dfdfb94d9e
Garrett Bearss <101658141+garrettbearss@users.noreply.github.com>
2023-11-08 14:22:40 -0500

    Update shield-related.js

--- src/shield-related.js
+++ src/shield-related.js
@@ -18,17 +18,24 @@ function energie(){// Generate an energy block every 5 seconds
 }
 
 function Shieldtime(){// Turns off invincibility mode at the end of the energy shield's duration and changes the shield's state
+  if(isPaused() == false){
   player.shield = false
   shieldOffSound.play(0, 1, 3);
   player.display();
   mode = 1;
   setTimeout(energie, 5000);
+  }
 }
 
 function ShieldCountdown(){ //Shield Countdown
   if(ShieldCT > 0){
-    ShieldCT--;
+    if(isPaused() == false){
+      ShieldCT--;
+    }
     setTimeout(ShieldCountdown, 1000)
+    if(ShieldCT == 0){
+      Shieldtime();
+    }
   }  
 }
 
@@ -39,7 +46,6 @@ function OpenShield(){ //  Open Shield
   player.display();// Change shield status and display shield
   mode = 5; //Toggle Invincible Mode
   ShieldCountdown();// Shield Countdown
-  setTimeout(Shieldtime, 5000*(energies));// Shield Duration
   energies = 0;// Empty energy
   energiesarray = [];// Empty energy
 }
@@ -49,12 +55,11 @@ function displayShieldInfo() {
    stroke(58, 127, 214);
    fill(205, 205, 205);
    rect(CANV_WIDTH*(677/720), CANV_HEIGHT*(116/400), CANV_WIDTH*(30/720), CANV_HEIGHT*(176/400), 5);
- 
+
  // Draws Energy Blocks
    for(var i = 0; i < 10; i++){
      stroke(0,0,0);
      fill(255, 156, 51);
      rect(CANV_WIDTH*(677/720), energiesarray[i], CANV_WIDTH*(30/720), CANV_HEIGHT*(15/400), 20);
    }
- 
 }
