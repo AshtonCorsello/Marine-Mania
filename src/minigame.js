09025bdc28ad5062aea0e260d419ea0d5b3f2bb4
Garrett Bearss <101658141+garrettbearss@users.noreply.github.com>
2023-11-13 17:24:35 -0500

    Update minigame.js

--- src/minigame.js
+++ src/minigame.js
@@ -4,20 +4,32 @@
 
 var death = true;
 var currentDead = false;
-var displaySize = 200;
-var displayX = CANV_WIDTH/2 - displaySize/2;
-var displayY = CANV_HEIGHT/2 - displaySize/2;
 var displayBox;
 
 function isFirstDeath(){ // Checks to see if this is the player's first death
   return death;
 }
 
-function isCurrentlyDead(){
+function isCurrentlyDead(){ // Checks to see if the player is currently dead
   return currentDead;
 }
 
 function minigameDisplay(){
-  removeElements(pauseText,pauseButton);
-  displaybox = rect(displayX, displayY, displaySize, displaySize);
+  // Remove when minigame is implemented
+  testpauseButton = createButton('Unpause');
+  testpauseButton.position(0, 0);
+  testpauseButton.size(CANV_WIDTH/6, CANV_HEIGHT/20);
+  testpauseButton.mousePressed(minigameEnd);
+  // Remove when minigame is implemented ^^^
+  // Implement Game below
+  displaybox = rect(CANV_WIDTH/2, CANV_HEIGHT/2, CANV_HEIGHT/2, CANV_HEIGHT/2); // Dedicates the space the on death minigame should be displayed in
+}
+
+function minigameEnd(){
+  removeElements(testpauseButton); // Remove when minigame is implemented
+  unpause();
+  minigameOpenShield(); // Gives the player a shield for 5 secs after death
+  currentDead = false;
+  death = false;
+  player.setHitFalse();
 }
