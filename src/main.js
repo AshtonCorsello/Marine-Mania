7512d677bb6c0d3d91858f57f608518f628fab26
Toby <131214504+tbaker60@users.noreply.github.com>
2023-11-29 22:35:58 -0500

    added new background

--- src/main.js
+++ src/main.js
@@ -73,7 +73,7 @@ function preload() {
    //mySound = loadSound('./src/BeepBox-Song.wav'); // load music file
    mySound = loadSound('./src/SFX/bgm1.wav');  // alternative BGM choice.  comment, and uncomment the above line to get other back.
    mainMenu = loadImage('./src/mainMenu.gif'); // load main menu gif
-   level1 = loadImage('./src/level1.gif'); // load level 1 gif
+   level1 = loadImage('./src/background1.2.gif'); // load level 1 gif
    gameover = loadImage('./src/gameover.png'); // load gameover file
    playerImg = loadImage('./src/img/boat1.0.png');
    drakcloude = loadImage('./src/img/cloude.png');
